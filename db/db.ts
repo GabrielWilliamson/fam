bf51965b5e398c0e8e555335b853cb586a0aef1f
Gabriel <gabrielwilliamson92@gmail.com>
2024-08-06 10:34:38 -0600

    add time utc and database

--- db/db.ts
+++ db/db.ts
@@ -2,16 +2,16 @@ import * as schema from "./schemas";
 import dotenv from "dotenv";
 dotenv.config();
 
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL!,
+// });
 
-export const db = drizzle(pool, { schema });
+// export const db = drizzle(pool, { schema });
 
-// import { drizzle } from "drizzle-orm/neon-http";
-// import { neon } from "@neondatabase/serverless";
-// const sql = neon(process.env.DATABASE_URL!);
-// export const db = drizzle(sql, { schema });
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
